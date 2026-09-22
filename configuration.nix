88a58cb60b4f3cb6031b5f8520df56742ddb6b09
Xiang Ji <xiangji.ucsd@gmail.com>
2026-09-22 09:17:45 -0400

    init

---
+++ configuration.nix
@@ -0,0 +1,43 @@
+{ user, ... }: 
+
+{
+  # Determinate already manages the Nix daemon, so nix-darwin shouldn't. 
+  nix.enable = false;
+  
+  nixpkgs.config.allowUnfree = true; # allow install packages from nix that are not free
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  system.primaryUser = user;
+  users.users.${user} = {
+    home = "/Users/${user}";
+  };
+  system.stateVersion = 6;
+  system.defaults = {
+    NSGlobalDomain = {
+      AppleInterfaceStyle = "Dark";
+      _HIHideMenuBar = true;  # auto-hide the menu bar
+      AppleShowAllExtensions = true;
+    };
+    dock.autohide = true;
+    finder.FXPreferredViewStyle = "Nlsv";  # list view by default
+  };
+
+  nix-homebrew = {
+    enable = true;
+    inherit user;
+  };
+  homebrew = {
+    enable = true;
+    onActivation.cleanup = "zap";  # remove anything not listed here
+    onActivation.autoUpdate = true;
+    onActivation.extraFlags = [ "--force" ];
+    brews = [
+      "herdr"
+    ];
+    casks = [
+      "wezterm"
+      "claude-code"
+      "codex"
+    ];
+  };
+}
